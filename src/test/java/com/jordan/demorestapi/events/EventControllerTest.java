f181b325fc5da7495867253496f39d7ad31fc7b4
jordan lee <leejs198@gmail.com>
2020-10-14 03:33:12 +0900

    DOCKER postgreSQL 설치 TEST REAL properties 분리

--- src/test/java/com/jordan/demorestapi/events/EventControllerTest.java
+++ src/test/java/com/jordan/demorestapi/events/EventControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 class EventControllerTest {
 
     @Autowired
---
+++ src/test/java/com/jordan/demorestapi/index/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.jordan.demorestapi.index;
+
+import com.jordan.demorestapi.common.RestDocsConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
+public class IndexControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void index() {
+
+    }
+}
